8d9c23045cd9c856d76ab8b7a17b5cb882d718bd
nkina1516 <nkina@siue.edu>
2024-02-05 16:04:39 -0600

    updated arguments.py

--- arguments.py
+++ arguments.py
@@ -1,1 +1,22 @@
 #Argparse module to parse input arguments
+import argparse
+
+def demo() -> None:
+    parser= argparse.ArgumentParser()
+    parser.add_argument(help="Enter a path",dest="input_path",type=str)
+    parser.add_argument(help="Enter a number",dest="number",type=int)
+    parser.add_argument(help="Enter a number",dest="double",type=float)
+    args=parser.parse_args()
+
+    path=args.input_path
+    num=args.number
+    num2=args.double
+
+    print("The path is: ",path)
+    print("the number is : ",num)
+    print("The double is: ", num2)
+
+if __name__=='__main__': 	
+    demo()
+
+
