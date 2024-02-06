b437842d61a992434c6c917f782088c98b789ec2
nkina1516 <nkina@siue.edu>
2024-02-05 23:01:34 -0600

    added a folder

---
+++ module1/thing1.py
@@ -0,0 +1,5 @@
+def hello_world():
+    print("You are inside of  module1")  
+    print("I am going to tell you about the highest rated movies on Rotten Tomatoes right now")
+def whatsnum1():
+    print("The first is Argyle")
