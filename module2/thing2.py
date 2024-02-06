878ce2f31a948c425240521831949beec6dcecf6
nkina1516 <nkina@siue.edu>
2024-02-05 23:02:16 -0600

    added a folder

---
+++ module2/thing2.py
@@ -0,0 +1,5 @@
+def hello_world():
+    print("You are inside of a module2")
+
+def whatsnum2():
+    print("The second most popular film right now is The Beekeper")
---
+++ module3/thing3.py
@@ -0,0 +1,6 @@
+#This is thing3.py
+def hello_world():
+    print("You are inside of module 3")
+
+def whatsnum3():
+    print("The third most popular movie right now is Anotomy of a Fall")
